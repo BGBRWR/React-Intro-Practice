fad664845dd1e19c8ab6c2c940d96d21d28a6c77
Austin Walker <bgbrwr@gmail.com>
2016-12-13 14:37:09 -0700

    Step 12 completed

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,15 +2,18 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./Landing.jsx')
 const Search = require('./Search.jsx')
-const {Router, Route, hashHistory} = require('react-router')
+const Layout = require('./Layout.jsx')
+const {Router, Route, IndexRoute, hashHistory} = require('react-router')
 // const Router = ReactRouter.Router
 // const Route = ReactRouter.Route
 // const hashHistory = ReactRouter.hashHistory
 
 const App = () => (
   <Router history={hashHistory}>
-    <Route path='/' component={Landing} />
-    <Route path='/search' component={Search} />
+    <Route path='/' component={Layout}>
+      <IndexRoute component={Landing} />
+      <Route path='/search' component={Search} />
+    </Route>
   </Router>
 )
 
